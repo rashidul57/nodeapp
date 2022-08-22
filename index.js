d4aa80a319710956514565217f679e91f32c0551
Rashidul Islam <mrashidbd2000@gmail.com>
2022-08-22 17:16:43 -0300

    updates

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // server.js
+require('dotenv').config()
 
 const express = require('express')
+
 const app = express()
 const port = process.env.PORT
-
 
 app.get('/', (req, res) => {
     res.send('Hello world Version 4')
 })
 
-app.listen(port, () => {
+app.listen(() => {
     console.log('Server is up on ' + port)
 })
