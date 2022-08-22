c0c6878bb0c31c1105049b75c7657e7a3d100399
Rashidul Islam <mrashidbd2000@gmail.com>
2022-08-22 11:25:20 -0300

    initial version

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world Version3')
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on 3000')
+})
